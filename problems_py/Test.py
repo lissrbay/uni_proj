17956baec8a07735a7405776d78f120e61067d49
Unknown <rbayliss964@gmail.com>
2017-11-25 09:25:25 +0300

    add pack_6

--- problems_py/Test.py
+++ problems_py/Test.py
@@ -1,12 +1,29 @@
-#ссылки на все задачки на первой странице попыток
+#ссылки на все когда либо решаемые задачки при вводе хендла
 import requests
 from bs4 import BeautifulSoup
-url = 'http://codeforces.com/submissions/NyanCake'
+
+username = str(input())
+url = 'http://codeforces.com/submissions/' + username
 r = requests.get(url)
+max_page = 0
 soup = BeautifulSoup(r.text, "lxml")
-with open('test.txt', 'w', encoding = 'utf-8') as output:
+
+for link in soup.find_all(attrs={"class" : "page-index"}):
+    s = link.find('a')
+    s2 = s.get("href").split('/')
+    max_page = max(max_page, int(s2[4]))
+	
+a = set()
+
+for i in range(0, max_page):
+    r = requests.get('http://codeforces.com/submissions/' + username + '/page/' + str(i))
+    soup = BeautifulSoup(r.text, "lxml")
+
     for link in soup.find_all('a'):
         s = link.get('href')
         if  s != None and len(s) > 12 and s.find('/problemset') != -1:
-            output.write('http://codeforces.com' + s)
-            output.write('\n')
+            a.add('http://codeforces.com' + s)
+			
+with open('test.txt', 'w', encoding = 'utf-8') as output:                
+    for i in a:
+        output.write(i + '\n')
