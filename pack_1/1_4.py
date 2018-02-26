c3f3e2c790b8a3f22d89e0c8d8bc10c379316dd6
nyancake <rbayliss964@gmail.com>
2018-02-26 09:50:30 +0300

    First task of java homework

--- pack_1/1_4.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-print("Введите строку для проверки на палиндромность: ")
-s = str(input())
-
-palindrome = (s[: len(s) / 2] == s[len(s) : len(s) / 2 - 1 + len(s) % 2 : -1])
-if palindrome:
-	print("Да.")
-else:
-	print("Нет.")
